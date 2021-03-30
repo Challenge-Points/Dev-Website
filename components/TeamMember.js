db3c43ffd2ef18c837ddb5b5c46ab622d5ba1cb6
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-03-30 17:25:42 +0100

    make team page react and not html in react form

---
+++ components/TeamMember.js
@@ -0,0 +1,16 @@
+import Image from "next/Image";
+
+const TeamMember = ({ userID, pfpURL }) => {
+	return (
+		<>
+			<td>
+				<Image href={`/users/${userID}`} src={`/../public/team/${pfpURL}`} alt={pfpURL} class="picture" width="200" height="200" />
+			</td>
+			<td>
+				<div class="margin-left-10"></div>
+			</td>
+		</>
+	);
+};
+
+export default TeamMember;
--- pages/team.js
+++ pages/team.js
@@ -1,102 +1,35 @@
-import Image from "next/Image"
+import Image from "next/Image";
+
+import TeamMember from "../components/TeamMember";
 
 const Team = () => {
-    return ( 
-        <div>
-        <h1>Team</h1>
-        <h2>Developers</h2>
-        <tbody>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/ThiJNmEnS.png" alt="ThiJNmEnS pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Aso.gif" alt="Aso pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/ActuallyThatsWrong.png" alt="ActuallyThatsWrong pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Lauriethefish.png" alt="Lauriethefish pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-            <tr><div class="margin-left-10"></div></tr>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/Sirspam.png" alt="Sirspam pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/アニメのくわ.png" alt="アニメのくわ pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-            <tr><div class="margin-left-10"></div></tr>
-        </tbody>
-        <h2>Ranking Team</h2>
-        <tbody>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/Owotsu.png" alt="Owotsu pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Abstr4kt.png" alt="Abstr4kt pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Clock.png" alt="Clock pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Flaime.png" alt="Flaime pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-            <tr><div class="margin-left-10"></div></tr>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/Galaxy99.png" alt="Galaxy99 pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Hairclip.png" alt="Hairclip pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/HELO.png" alt="HELO pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/MasterSlayer379.gif" alt="MasterSlayer379 pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-            <tr><div class="margin-left-10"></div></tr>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/Phytotitan_10.png" alt="Phytotitan_10 pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Plasim.png" alt="Plasim pfp" class="picture" width="200" height="200"/>
-                </td>
-                <td><div class="margin-left-10"></div></td>
-                <td>
-                    <Image href="/users/580425653325791272" src="/../public/team/Salad.png" alt="Salad pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-        </tbody>
-        <h2>Honorable Mentions</h2>
-        <tbody>
-            <tr>
-                <td>
-                    <Image href="/users/490534335884165121" src="/../public/team/LicensedCrime.png" alt="LicensedCrime pfp" class="picture" width="200" height="200"/>
-                </td>
-            </tr>
-        </tbody>
-        </div>
-     );
-}
- 
+	var devs = [
+		{ pfpURL: "Aso.gif", userID: "580425653325791272" },
+		{ pfpURL: "ThiJNmEnS.png", userID: "490534335884165121" },
+		{ pfpURL: "ActuallyThatsWrong.png", userID: "580425653325791272" },
+		{ pfpURL: "Lauriethefish.png", userID: "580425653325791272" },
+		{ pfpURL: "Sirspam.png", userID: "490534335884165121" },
+	];
+
+	return (
+		<div>
+			<h1>Team</h1>
+			<h2>Developers</h2>
+			<tbody>
+				{devs.map((dev) => {
+					return <TeamMember pfpURL={dev.pfpURL} userID={dev.userID} />;
+				})}
+			</tbody>
+			<h2>Ranking Team</h2>
+			<tbody>
+				<tr></tr>
+			</tbody>
+			<h2>Honorable Mentions</h2>
+			<tbody>
+				<tr></tr>
+			</tbody>
+		</div>
+	);
+};
+
 export default Team;
