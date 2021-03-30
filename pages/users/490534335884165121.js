e3e87781b7f934e3acb0d875e24ae0581e9705e5
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-03-30 20:24:01 +0100

    heroku 2

--- pages/users/490534335884165121.js
+++ pages/users/490534335884165121.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import AboutMe from "../../components/AboutMe";
 
 const User490534335884165121 = () => {
