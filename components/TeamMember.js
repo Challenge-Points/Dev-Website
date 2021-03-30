afef13fde0d830fd6f23295bd57ecf7901a1edcd
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-03-30 17:43:30 +0100

    fixed console spam and removed unused api folder

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -4,7 +4,7 @@ const TeamMember = ({ userID, pfpURL }) => {
 	return (
 		<>
 			<td>
-				<Image href={`/users/${userID}`} src={`/../public/team/${pfpURL}`} alt={pfpURL} class="picture" width="200" height="200" />
+				<Image href={`/users/${userID}`} src={`/team/${pfpURL}`} alt={pfpURL} class="picture" width="200" height="200" />
 			</td>
 			<td>
 				<div class="margin-left-10"></div>
