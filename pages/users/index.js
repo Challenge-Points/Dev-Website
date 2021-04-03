3412b5e0bc3cea7a09948bdcf1f3a98ddd332fab
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-04-03 15:54:32 +0100

    users page :)

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,17 +1,44 @@
 import Link from "next/link";
 
-const Users = () => {
+import styles from "../../styles/Users.module.css";
+
+export const getServerSideProps = async () => {
+	var res = await fetch("http://localhost/api/users/global/1");
+	var data = await res.json();
+
+	return {
+		props: { data },
+	};
+};
+
+const Users = (props) => {
 	return (
 		<div>
 			<h1>Users</h1>
-			<tbody>
-				<tr>
-					<Link href="/users/490534335884165121"><a>ThiJNmEnS</a></Link>
-				</tr>
-				<tr>
-					<Link href="/users/580425653325791272"><a>Aso</a></Link>
-				</tr>
-			</tbody>		
+			<table className={styles.userTable}>
+				<thead>
+					<td>
+						<b> # </b>
+					</td>
+					<td>
+						<b> Username </b>
+					</td>
+					<td>
+						<b>CP</b>
+					</td>
+				</thead>
+				<tbody>
+					{props.data.map((user) => {
+						return (
+							<tr key={user.id}>
+								<td> {user.global.toString()} </td>
+								<td> {user.username} </td>
+								<td> {user.cp.toString()} </td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
 		</div>
 	);
 };
