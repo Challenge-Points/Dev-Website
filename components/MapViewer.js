05b9fd0d6eac2940aa81fe71effdaeb530bab542
thijnmens <thijnsmulders04@gmail.com>
2021-04-06 11:46:19 +0200

    Map info pages are getting a fancy upgrade

---
+++ components/MapViewer.js
@@ -0,0 +1,17 @@
+const MapViewer = (props) => {
+    var id = 0
+    var diff = 0
+    var mode = 0
+    id = props.id;
+    diff = props.diff;
+    mode = props.diff;
+    var url = "https://skystudioapps.com/bs-viewer/?id=" + id + "&difficulty=" + diff + "&mode=" + mode;
+    return ( 
+        <div>
+            <iframe src={url} width="960" height="540" scrolling="no"/>
+            <h6>BSViewer is powered by skystudioapps.com</h6>
+        </div>
+     );
+}
+ 
+export default MapViewer;
--- pages/maps/0343313f585c228c0fd4daf3ae976a491a184f20.js
+++ pages/maps/0343313f585c228c0fd4daf3ae976a491a184f20.js
@@ -1,7 +1,12 @@
+import MapLoaderboard from "../../components/MapLeaderboard";
+import MapViewer from "../../components/MapViewer"
+
 const Map0343313f585c228c0fd4daf3ae976a491a184f20 = () => {
     return ( 
         <div>
-            <h1>0343313f585c228c0fd4daf3ae976a491a184f20</h1>
+            <h4><h1>Ferrari</h1>by midddd</h4>
+            <MapLoaderboard hash="0343313f585c228c0fd4daf3ae976a491a184f20"/>
+            <MapViewer id="fcde" diff="1"/>
         </div>
      );
 }
