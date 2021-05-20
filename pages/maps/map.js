780c8c5245a104086b2443d890aae81b7f2839b1
thijnmens <thijnsmulders04@gmail.com>
2021-05-20 10:06:16 +0200

    Yes i know its broken, i just can't figure it out pepeHands

--- pages/maps/map.js
+++ pages/maps/map.js
@@ -4,31 +4,48 @@ import { useRouter } from 'next/router'
 //import ClipboardJS from 'clipboard'
 
 function bsrClipboard() {
-    const router = useRouter();
     var bsr = router.query.key
     bsr.execCommand("copy");
     alert("Copied bsr")
 }
 
+async function getMapInfo(router) { 
+    if(router.isReady) {
+        var res = await fetch(`https://challenge-points-dev.herokuapp.com/api/maps/hash/${router.query.hash}`);
+        var data = await res.json();
+    }
+    return {
+        props: { data }
+    }
+};
+
 const Map = () => {
     const router = useRouter();
+    var props = getMapInfo(router);
+    props.then((map) => console.log(map));
+    var data;
     //var clipboard = new ClipboardJS('.btn');
-    return ( 
+    return (
         <div>
-            <h4><h1>{router.query.name}</h1>by {router.query.mapper}</h4>
+            {props.then((map) => {
             <div>
-                <clipboard />
-                <button title="Copy !bsr" className="btn">!</button>
-                <a href={`https://beatsaver.com/beatmap/${router.query.key}`} target="_blank">
-                    <button title="Beatsaver Link" className="btn">
-                        <img src="/icons/BeatSaverLogo.png" className="btn_icon"/>
-                    </button>
-                </a>
+            <h1>{map.m_n}</h1>
+            <h4>by {map.mr_n}</h4>
+                <div>
+                    <clipboard />
+                    <button title="Copy !bsr" className="btn">!</button>
+                    <a href={`https://beatsaver.com/beatmap/${map.key}`} target="_blank">
+                        <button title="Beatsaver Link" className="btn">
+                            <img src="/icons/BeatSaverLogo.png" className="btn_icon"/>
+                        </button>
+                    </a>
+                </div>
+                <MapLeaderboard hash={router.query?.hash}/>
+                <MapViewer id={map.key} diff={router.query?.diff} mode={router.query?.mode}/>
             </div>
-            <MapLeaderboard hash={router.query.hash}/>
-            <MapViewer id={router.query.key} diff={router.query.diff} mode={router.query.mode}/>
+            })}
         </div>
-     );
-}
+    );
+};
  
 export default Map;
