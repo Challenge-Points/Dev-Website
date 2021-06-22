0497f0abc321109ffb3b5a83f15611749177dba4
thijnmens <thijnsmulders04@gmail.com>
2021-06-22 18:26:52 +0200

    Fixed the team page and switched back to the old method of the map page loading

--- pages/maps/map.js
+++ pages/maps/map.js
@@ -8,7 +8,7 @@ function bsrClipboard() {
     bsr.execCommand("copy");
     alert("Copied bsr")
 }
-
+/*
 async function getMapInfo(router) { 
     if(router.isReady) {
         var res = await fetch(`https://challenge-points-dev.herokuapp.com/api/maps/hash/${router.query.hash}`);
@@ -17,33 +17,30 @@ async function getMapInfo(router) {
     return {
         props: { data }
     }
-};
+};*/
 
-const Map = () => {
+const Map = (props) => {
     const router = useRouter();
-    var props = getMapInfo(router);
-    props.then((map) => console.log(map));
-    var data;
+    //var props = getMapInfo(router);
+    //props.data.then((map) => console.log(map));
     //var clipboard = new ClipboardJS('.btn');
     return (
         <div>
-            {props.then((map) => {
             <div>
-            <h1>{map.m_n}</h1>
-            <h4>by {map.mr_n}</h4>
+                <h1>{router.query?.name}</h1>
+                <h4>by {router.query?.mapper}</h4>
                 <div>
                     <clipboard />
                     <button title="Copy !bsr" className="btn">!</button>
-                    <a href={`https://beatsaver.com/beatmap/${map.key}`} target="_blank">
-                        <button title="Beatsaver Link" className="btn">
-                            <img src="/icons/BeatSaverLogo.png" className="btn_icon"/>
-                        </button>
+                    <a href={`https://beatsaver.com/beatmap/${router.query?.key}`} target="_blank">
+                    <button title="Beatsaver Link" className="btn">
+                        <img src="/icons/BeatSaverLogo.png" className="btn_icon"/>
+                    </button>
                     </a>
                 </div>
                 <MapLeaderboard hash={router.query?.hash}/>
-                <MapViewer id={map.key} diff={router.query?.diff} mode={router.query?.mode}/>
+                <MapViewer id={router.query?.key} diff={router.query?.diff} mode={router.query?.mode}/>
             </div>
-            })}
         </div>
     );
 };
